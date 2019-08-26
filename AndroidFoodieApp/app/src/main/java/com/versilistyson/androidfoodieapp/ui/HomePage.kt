053667cca0a1fd8d435da3d564eb471cbbfd7cfb
Versilis Tyson <VersilisTyson@gmail.com>
2019-08-26 18:20:56 -0400

    Adds Profile Stats and FAB to UI

---
+++ AndroidFoodieApp/app/src/main/java/com/versilistyson/androidfoodieapp/ui/HomePage.kt
@@ -0,0 +1,13 @@
+package com.versilistyson.androidfoodieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.versilistyson.androidfoodieapp.R
+
+class HomePage : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
